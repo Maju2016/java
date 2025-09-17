d3623eb345337964c5d93fe1ebeef5d5d5d4218a
Maria <mjuliamedeiros.2016@gmail.com>
2025-09-17 17:06:12 -0300

    Arrays

---
+++ colecoes/src/colecoes/Lista.java
@@ -0,0 +1,56 @@
+package colecoes;
+
+import java.util.ArrayList;
+
+public class Lista {
+
+	public static void main(String[] args) {
+
+		ArrayList<Integer> numeros = new ArrayList<Integer>();
+		
+		numeros.add(5);
+		numeros.add(7);
+		numeros.add(8);
+		numeros.add(10);
+		numeros.add(5);
+		
+		System.out.println(numeros);
+		
+		System.out.println("Exibir o elemento na posicao 2: " + numeros.get(2));
+		
+		System.out.println("Exibir o indice do elemento na posicao 10: " + numeros.indexOf(10));
+		
+		numeros.set(numeros.indexOf(10), 4);
+		
+		System.out.println(numeros);
+		
+		System.out.println("Exibir o indice do elemento 10: " + numeros.indexOf(10));
+		
+		numeros.remove(numeros.indexOf(5));
+		
+		System.out.println(numeros);
+		
+		for(int indice = 0; indice < numeros.size(); indice++) {
+			
+		}
+		
+		//For Each, isso substitui o que esta em cima
+		for(int numero : numeros) {
+			System.out.println(numero);
+		}
+		
+		System.out.println("Tamanho da lista: " + numeros.size());
+		
+		System.out.println("A lista está vazia? " + numeros.isEmpty());
+		
+		System.out.println("O numero 8 esta na lista? " + numeros.contains(8));
+		
+		//Limpar a lista
+		numeros.clear();
+		
+		System.out.println("A lista esta vazia? " + numeros.isEmpty());
+		
+
+	}
+
+}
